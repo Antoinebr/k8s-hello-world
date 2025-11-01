eefe30e6cf4ff76ef32e8833725624ca73246ecb
Antoine  Brossault <antoinebrossault@gmail.com>
2025-11-01 14:13:48 +0100

    v2

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-// A "VERSION" variable we can change later
-const VERSION = '1.0.0';
+// A "VERSION" variable we can change later 
+const VERSION = '2.0.0';
 
 app.get('/', (req, res) => {
   res.send(`Hello from Kubernetes! Version: ${VERSION}`);
@@ -13,3 +13,4 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`);
 });
+
